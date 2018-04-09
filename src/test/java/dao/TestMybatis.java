b86e6a1d76880bdb1c4dce42bf97e57674093915
S <1004593281@qq.com>
2018-04-09 20:12:36 +0800

    test

--- src/test/java/dao/TestMybatis.java
+++ src/test/java/dao/TestMybatis.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.cornelius.bean.User;
 import com.cornelius.service.UserService;
-@ContextConfiguration({"/application*.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath*:/application*.xml"})
 public class TestMybatis {
 
 	@Autowired

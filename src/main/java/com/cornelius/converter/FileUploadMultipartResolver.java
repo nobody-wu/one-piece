c3be4ffcf267b9c2d15ef14c6f941638bee899a2
jiapeng.wu <jiapeng.wu@yuntu-inc.com>
2018-04-25 11:49:17 +0800

    form-data格式上传

---
+++ src/main/java/com/cornelius/converter/FileUploadMultipartResolver.java
@@ -0,0 +1,44 @@
+package com.cornelius.converter;
+
+import org.apache.commons.fileupload.*;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+/**
+ * 
+ * @author wujiapeng
+ * 
+ * @Description:文件上传处理类
+ * 
+ *
+ */
+@SuppressWarnings("unchecked")
+public class FileUploadMultipartResolver extends CommonsMultipartResolver {
+
+	@Override
+	protected MultipartParsingResult parseRequest(HttpServletRequest request) throws MultipartException {
+		String encoding = "utf-8";
+		FileUpload fileUpload = prepareFileUpload(encoding);
+		final HttpSession session = request.getSession();
+		fileUpload.setProgressListener(new ProgressListener() {
+			public void update(long pBytesRead, long pContentLength, int pItems) {
+				int percent = (int) (((float) pBytesRead / (float) pContentLength) * 100);
+				session.setAttribute("percent", percent);
+			}
+		});
+		try {
+			List<FileItem> fileItems = ((ServletFileUpload) fileUpload).parseRequest(request);
+			return parseFileItems(fileItems, encoding);
+		} catch (FileUploadBase.SizeLimitExceededException ex) {
+			throw new MaxUploadSizeExceededException(fileUpload.getSizeMax(), ex);
+		} catch (FileUploadException ex) {
+			throw new MultipartException("Could not parse multipart servlet domain", ex);
+		}
+	}
+}
